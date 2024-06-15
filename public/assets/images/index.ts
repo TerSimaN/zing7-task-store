6ace663aaea65c9b235bbfed4b8b65a0d0223dc2
TerSimaN <mcsiman998@gmail.com>
2024-06-15 22:03:34 +0300

    Added images import/export file

---
+++ public/assets/images/index.ts
@@ -0,0 +1,38 @@
+import hero_image_1 from './hero_image_1.png';
+import promo_banner_image from './promo_banner_image.png';
+
+import airpods_2nd_gen from './airpods_2nd_gen.png';
+import apple_watch_4_2020 from './apple_watch_4_2020.png';
+import beats_by_dre_c_3450 from './beats_by_dre_c_3450.png';
+import garmin_watch_fit_x from './garmin_watch_fit_x.png';
+import harman_kardon_speaker from './harman_kardon_speaker.png';
+import iphone_xs_max_pro from './iphone_xs_max_pro.png';
+import samsung_galaxy_watch_3 from './samsung_galaxy_watch_3.png';
+import women_yellow_turtleneck from './women_yellow_turtleneck.png';
+
+import product_list_1_item from './product_list_1_item.png';
+import product_list_2_item from './product_list_2_item.png';
+import product_list_3_item from './product_list_3_item.png';
+
+import blog_image_1 from './blog_image_1.png';
+import blog_image_2 from './blog_image_2.png';
+import blog_image_3 from './blog_image_3.png';
+
+export {
+    hero_image_1,
+    promo_banner_image,
+    airpods_2nd_gen,
+    apple_watch_4_2020,
+    beats_by_dre_c_3450,
+    garmin_watch_fit_x,
+    harman_kardon_speaker,
+    iphone_xs_max_pro,
+    samsung_galaxy_watch_3,
+    women_yellow_turtleneck,
+    product_list_1_item,
+    product_list_2_item,
+    product_list_3_item,
+    blog_image_1,
+    blog_image_2,
+    blog_image_3,
+}
