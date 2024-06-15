ee08691baae979d2208cf3c62f72a2a50c098900
TerSimaN <mcsiman998@gmail.com>
2024-06-15 22:01:15 +0300

    Added icon import/export file

---
+++ public/assets/icons/index.ts
@@ -0,0 +1,65 @@
+import facebook_icon from './facebook_icon.svg';
+import twitter_icon from './twitter_icon.svg';
+import youtube_icon from './youtube_icon.svg';
+import instagram_icon from './instagram_icon.svg';
+import github_icon from './github_icon.svg';
+import linkedin_icon from './linkedin_icon.svg';
+
+import arrow_down_icon from './arrow_down_icon.svg';
+import arrow_right_icon from './arrow_right_icon.svg';
+
+import us_flag_icon from './us_flag_icon.svg';
+import fr_flag_icon from './fr_flag_icon.svg';
+import de_flag_icon from './de_flag_icon.svg';
+import es_flag_icon from './es_flag_icon.svg';
+import gb_flag_icon from './gb_flag_icon.svg';
+import it_flag_icon from './it_flag_icon.svg';
+import ca_flag_icon from './ca_flag_icon.svg';
+import cn_flag_icon from './cn_flag_icon.svg';
+import jp_flag_icon from './jp_flag_icon.svg';
+
+import android_phone_icon from './android_phone_icon.svg';
+import basket_ball_icon from './basket_ball_icon.svg';
+import camera_icon from './camera_icon.svg';
+import controller_icon from './controller_icon.svg';
+import monitor_icon from './monitor_icon.svg';
+import undershirt_icon from './undershirt_icon.svg';
+
+import asus_icon from './asus_icon.svg';
+import xiaomi_icon from './xiaomi_icon.svg';
+import samsung_icon from './samsung_icon.svg';
+import sony_icon from './sony_icon.svg';
+import wacom_icon from './wacom_icon.svg';
+import apple_icon from './apple_icon.svg';
+
+export {
+    facebook_icon,
+    twitter_icon,
+    youtube_icon,
+    instagram_icon,
+    github_icon,
+    linkedin_icon,
+    arrow_down_icon,
+    arrow_right_icon,
+    us_flag_icon,
+    fr_flag_icon,
+    de_flag_icon,
+    es_flag_icon,
+    gb_flag_icon,
+    it_flag_icon,
+    ca_flag_icon,
+    cn_flag_icon,
+    jp_flag_icon,
+    android_phone_icon,
+    basket_ball_icon,
+    camera_icon,
+    controller_icon,
+    monitor_icon,
+    undershirt_icon,
+    asus_icon,
+    xiaomi_icon,
+    samsung_icon,
+    sony_icon,
+    wacom_icon,
+    apple_icon,
+}
